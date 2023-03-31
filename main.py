9922a45fa16228a656374341dec6c924f12f9f0a
Daniil <danya055555@gmail.com>
2023-04-01 02:52:46 +0300

    echo-bot

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(['start', 'help'])
+@dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm TestBot!\nPowered by aiogram.")
+
+'''
+Если нужно ответить на сообщение - reply
+Если просто отправить - answer
+'''
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
